7c6c79cc42ce3144f2fcb7ff947fd973bb9ec80c
Sven Pradela <svenpra2013@gmail.com>
2025-11-08 21:46:12 +0100

    ...

--- src/main/java/com/example/demo/controller/EquipmentController.java
+++ src/main/java/com/example/demo/controller/EquipmentController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/equipment/")
+@RequestMapping("equipment/")
 public class EquipmentController {
 
     private EquipmentService equipmentService;
