ee344abccbc81bcf314a5b9a85bdea54f67b663d
Sven Pradela <svenpra2013@gmail.com>
2026-02-03 21:45:12 +0100

    fix for update flag password_changed

--- src/main/resources/db/changelog/password_changes_trigger_01.sql
+++ src/main/resources/db/changelog/password_changes_trigger_01.sql
@@ -1,6 +1,6 @@
-CREATE OR REPLACE FUNCTION log_password_changes()
-RETURNS TRIGGER AS
-$$
+
+CREATE OR REPLACE FUNCTION handle_user_password_logic()
+RETURNS TRIGGER AS $$
 BEGIN
     IF (TG_OP = 'INSERT') OR (OLD.password IS DISTINCT FROM NEW.password) THEN
         INSERT INTO user_password_changes (
@@ -18,11 +18,20 @@ BEGIN
             NOW()
         );
 END IF;
+
+    IF (TG_OP = 'UPDATE') THEN
+        IF (OLD.password IS DISTINCT FROM NEW.password) THEN
+            NEW.changed_password := TRUE;
+END IF;
+
+END IF;
+
 RETURN NEW;
 END;
 $$ LANGUAGE plpgsql;
 
-CREATE TRIGGER trigger_user_password_update
-    AFTER INSERT OR UPDATE OF password ON users
+CREATE TRIGGER trigger_user_management
+    BEFORE INSERT OR UPDATE OF password ON users
     FOR EACH ROW
-    EXECUTE FUNCTION log_password_changes();
+    EXECUTE FUNCTION handle_user_password_logic();
+
