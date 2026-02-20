e017166dda186f63c80caff05d9adcae364c5ba1
Yves, Priester <ypriester@gk-software.com>
2026-02-20 15:45:11 +0100

    non-functional change password dialog

--- src/main/resources/static/js/userProfile.js
+++ src/main/resources/static/js/userProfile.js
@@ -1,5 +1,8 @@
 const STATUS_OK = 200;
-
+const PW_DIALOG = document.getElementById("dialog-new-password");
+const PW_DIALOG_INPUT_OLD = document.getElementById("dialog-new-password-input-old-pw");
+const PW_DIALOG_INPUT_NEW = document.getElementById("dialog-new-password-input-new-pw");
+const PW_DIALOG_INPUT_NEW_CHECK = document.getElementById("dialog-new-password-input-new-pwCheck");
 
 async function changeValue(field,fieldFrontendName, firstName) {
     let userInput = await notificationDialog.showSingleInput("Neuer " + fieldFrontendName + ":", firstName);
@@ -18,4 +21,19 @@ async function changeValue(field,fieldFrontendName, firstName) {
     } catch (error) {
         alert(error.message);
     }
+}
+
+async function showChangePasswordDialog() {
+    PW_DIALOG.setAttribute("open","true");
+}
+
+async function sendPwChangeRequest() {
+
+}
+
+function closePwDialog() {
+    PW_DIALOG.setAttribute("open","false");
+    PW_DIALOG_INPUT_OLD.value = "";
+    PW_DIALOG_INPUT_NEW.value = "";
+    PW_DIALOG_INPUT_NEW_CHECK.value = "";
 }
